ebe4b2b82d8af637e1719a748c0ed050f5bf4319
Yanasbruh <denadewa9@gmail.com>
2022-09-04 19:47:25 +0800

    >,,<

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ let nav = document.querySelector("nav ul");
 menuToggle.addEventListener("click", function () {
   nav.classList.toggle("slide");
 });
+
+window.addEventListener("scroll", function () {
+  var nav = document.querySelector("nav");
+  nav.classList.toggle("hidden", window.scrollY > 0);
+});
